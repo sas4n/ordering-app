076ce2d718900163d722bcc1f192600ac2faea85
Rahim Pormah <sas4n.porm4h@gmail.com>
2024-07-07 15:19:00 +0200

    dto classes are now records

--- productService/src/main/java/com/myShoppingApp/dto/ProductRequest.java
+++ productService/src/main/java/com/myShoppingApp/dto/ProductRequest.java
@@ -2,18 +2,6 @@ package com.myShoppingApp.dto;
 
 import java.math.BigDecimal;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class ProductRequest {
- private String name;
-    private String description;
-    private BigDecimal price;
-
+public record ProductRequest(String name, String description, BigDecimal price) {
+ 
 }
--- productService/src/main/java/com/myShoppingApp/dto/ProductResponse.java
+++ productService/src/main/java/com/myShoppingApp/dto/ProductResponse.java
@@ -2,18 +2,7 @@ package com.myShoppingApp.dto;
 
 import java.math.BigDecimal;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Data
-public class ProductResponse {
-     private String id;
-    private String name;
-    private String description;
-    private BigDecimal price;
+public record ProductResponse(String id, String name, String description, BigDecimal price) {
+     
 }
