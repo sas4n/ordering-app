8309b144e53c6e7d0e2a408219b9312db0ad3339
Rahim Pormah <sas4n.porm4h@gmail.com>
2024-07-11 11:44:33 +0200

    Docker images updated

--- orderService/src/main/java/com/myShoppingApp/orderService/service/Service.java
+++ orderService/src/main/java/com/myShoppingApp/orderService/service/Service.java
@@ -2,6 +2,7 @@ package com.myShoppingApp.orderService.service;
 
 import java.util.UUID;
 
+import com.myShoppingApp.orderService.client.IInventoryClient;
 import com.myShoppingApp.orderService.dto.OrderRequest;
 import com.myShoppingApp.orderService.dto.OrderResponse;
 import com.myShoppingApp.orderService.model.Order;
@@ -16,19 +17,26 @@ import lombok.extern.slf4j.Slf4j;
 public class Service {
 
     private final IRepository iRepository;
+    private final IInventoryClient iInventoryClient;
 
     public OrderResponse placeNewOrder(OrderRequest orderRequest) {
-        Order newOrder = Order.builder()
-                .orderRefrence(UUID.randomUUID().toString())
-                .skuCode(orderRequest.skuCode())
-                .price(orderRequest.price())
-                .quantity(orderRequest.quantity())
-                .build();
+        boolean resopnseFromInventory = iInventoryClient.isAvalable(orderRequest.skuCode(), orderRequest.quantity());
+        if (resopnseFromInventory) {
+            Order newOrder = Order.builder()
+                    .orderRefrence(UUID.randomUUID().toString())
+                    .skuCode(orderRequest.skuCode())
+                    .price(orderRequest.price())
+                    .quantity(orderRequest.quantity())
+                    .build();
 
-        iRepository.save(newOrder);
+            iRepository.save(newOrder);
 
-        log.info("order {} is saved", newOrder.getId());
-        return new OrderResponse(newOrder.getId(), newOrder.getOrderRefrence(), newOrder.getSkuCode(),
-                newOrder.getPrice(), newOrder.getQuantity());
+            log.info("order {} is saved", newOrder.getId());
+            return new OrderResponse(newOrder.getId(), newOrder.getOrderRefrence(), newOrder.getSkuCode(),
+                    newOrder.getPrice(), newOrder.getQuantity());
+        }
+        throw new RuntimeException(
+                "The quantity of the requested product with sku code " + orderRequest.skuCode() + " is not enought");
+
     }
 }
