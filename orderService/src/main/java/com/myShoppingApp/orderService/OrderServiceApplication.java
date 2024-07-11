0a0691cfc284cf90d97839418591798e0b63e985
Rahim Pormah <sas4n.porm4h@gmail.com>
2024-07-11 20:19:35 +0200

    Pom rebuild due to some errors

--- orderService/src/main/java/com/myShoppingApp/orderService/OrderServiceApplication.java
+++ orderService/src/main/java/com/myShoppingApp/orderService/OrderServiceApplication.java
@@ -2,10 +2,10 @@ package com.myShoppingApp.orderService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@FeignClient
+@EnableFeignClients
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
