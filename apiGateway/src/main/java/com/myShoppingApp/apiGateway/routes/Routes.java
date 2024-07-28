d7c21ea5a3b5860c72cc4a2a643230287ca22080
Rahim Pormah <sas4n.porm4h@gmail.com>
2024-07-28 22:42:16 +0200

    swagger added to gateway

--- apiGateway/src/main/java/com/myShoppingApp/apiGateway/routes/Routes.java
+++ apiGateway/src/main/java/com/myShoppingApp/apiGateway/routes/Routes.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import static org.springframework.web.servlet.function.RequestPredicates.path;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.setPath;
 
 @Configuration
 public class Routes {
@@ -19,6 +20,14 @@ public class Routes {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
+        return route("product-service-swagger")
+                .route(path("/aggregate/product-service/v3/api-docs"), http("http://localhost:8080"))
+                .before(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
     public RouterFunction<ServerResponse> orderServiceRoute() {
         return route("order-service")
                 .route(path("/api/order/newOrder"), http("http://localhost:8081"))
@@ -26,9 +35,25 @@ public class Routes {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
+        return route("order-service-swagger")
+                .route(path("/aggregate/order-service/v3/api-docs"), http("http://localhost:8081"))
+                .before(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
     public RouterFunction<ServerResponse> inventoryServiceRoute() {
         return route("inventory-service")
                 .route(path("/api/inventory"), http("http://localhost:8082"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute() {
+        return route("inventory-service-swagger")
+                .route(path("/aggregate/inventory-service/v3/api-docs"), http("http://localhost:8082"))
+                .before(setPath("/api-docs"))
                 .build();
     }
 }
